1b8fb4a60238ad5d32a0e2c376dfd45867e8d959
kelcy0806 <matsui.kei3918@gmail.com>
2024-01-06 14:19:58 +0900

    1st commmit

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+import requests
+from PIL import Image
+
+st.title('pokemon similer')
+
+st.sidebar.header('Input Features')
+your_height = st.sidebar.slider('your height (m)', min_value=1.00, max_value=2.00, step=0.01)
+your_weight = st.sidebar.slider('your weight (kg)', min_value=0.0, max_value=200.0, step=0.1)
+
+your_feature ={
+    "your_height": your_height,
+    "your_weight": your_weight
+}
+
+
+
+if st.sidebar.button("GEUSS!!"):
+    # 入力された説明変数の表示
+    st.write('## Input Value')
+    your_df = pd.DataFrame(your_feature, index=["data"])
+    st.write(your_df)
+
+    # 予測の実行
+    response = requests.post("http://localhost:8000/guess", json=your_feature)
+    pokemon = response.json()["Pokemon"]
+    pokemon_image = response.json()["pokemon_image"]
+
+
+    # 予測結果の出力
+    st.write('## Result')
+    st.write('あなたに似ているポケモンはきっと',str(pokemon),'です!')
+
+    # 予測結果の表示
+    st.write('## Prediction')
+    st.write(pokemon)
+
+    # 予測画像の出力
+    st.write(str(pokemon_image))
+    st.write(repr(str(pokemon_image)))
+    #image = Image.open(repr(str(pokemon_image)))
+    image = Image.open("C:\\Users\\81805\\Desktop\\ポケモン＿練習用\\1.png")
+    #image = Image.open("https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/1.png")
+    st.image(image, caption='サンプル',use_column_width=True)
+
+
---
+++ main.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from fastapi import FastAPI
+from pydantic import BaseModel
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+from typing import Generic, TypeVar
+
+
+# インスタンス化
+app = FastAPI()
+
+# 入力するデータ型の定義
+class your_feature(BaseModel):
+     your_height: float
+     your_weight: float
+
+# 出力するデータ型の定義
+class pokemon_data(BaseModel):
+    id: float
+    name: int
+    type1: str
+    type2: str
+    height: float
+    weight: float
+
+#T = TypeVar('T')
+
+#class your_feature(Generic[T]):
+    # クラスの実装
+    #your_height: float
+    #your_weight: float
+
+# 学習済みのモデルの読み込み
+pokemon_df = pd.read_csv("C:\\Users\\81805\\Desktop\\ポケモン＿練習用\\pokemon_data2.csv")
+
+# トップページ
+@app.get('/')
+def index():
+    return {"Pokemon": 'pokemon_prediction'}
+
+@app.get('/test')
+def test():
+    return {"test": your_feature["your_height"]}
+
+
+# POST が送信された時（入力）と予測値（出力）の定義
+@app.post('/guess')
+def make_predictions(features: your_feature):
+    pokemon_df['差の総和'] = abs(pokemon_df['高さ'] - features.your_height) + abs(pokemon_df['重さ'] - features.your_weight)
+    #pokemon_df['差の総和'] = abs(pokemon_df['高さ'] - your_feature["your_height"]) + abs(pokemon_df['重さ'] - your_feature["your_weight"])
+    closest_pokemon = pokemon_df.loc[pokemon_df['差の総和'].idxmin()]
+
+    return({'Pokemon':str(closest_pokemon['名前']),'pokemon_image':str(closest_pokemon['画像URL'])})
